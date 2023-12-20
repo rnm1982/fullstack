f5c1cf51f8d88747672ffe1ccc3861dddbe1b5a3
Ravindra <ravindra4mahajan@gmail.com>
2023-12-20 09:40:39 +0530

    git commands added

--- JS Fundamentals/variable.js
+++ JS Fundamentals/variable.js
@@ -27,3 +27,7 @@ because of issue in block scope and functional scope
 
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// git add .
+// git commit -m "message"
+// git push
