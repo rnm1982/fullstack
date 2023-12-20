cb358e2cfdd6589840597dae7b693b3a92f9e703
Ravindra <ravindra4mahajan@gmail.com>
2023-12-20 09:51:37 +0530

    various Datatypes

---
+++ JS Fundamentals/datatypes.js
@@ -0,0 +1,17 @@
+//  Primitive
+
+//  (call by Value)7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null  // null is absence of value
+let userEmail;
+// console.log(userEmail);
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3456543576654356754n
