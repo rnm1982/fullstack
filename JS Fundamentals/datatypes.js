556322c9f9bca3cc986f9d9e63b086db2959a1fd
Ravindra <ravindra4mahajan@gmail.com>
2023-12-20 10:05:01 +0530

    Nonprimitive

--- JS Fundamentals/datatypes.js
+++ JS Fundamentals/datatypes.js
@@ -15,3 +15,28 @@ const anotherId = Symbol('123')
 console.log(id === anotherId);
 
 const bigNumber = 3456543576654356754n
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+    marks: 92
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+// const a=122
+// console.log(typeof a)
+const name="Reyansh";
+console.log(typeof name);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// stack(primitive) , heap(NonPrimitive)
